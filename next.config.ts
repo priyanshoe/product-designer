ca482c875db75d42d045d33213527b55b8dcb832
priyanshoe <priyanshurahate@gmail.com>
2025-08-12 22:43:29 +0530

    static changed

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-    output: 'export',
-};
-
-export default nextConfig;
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+ 
+}
+ 
+module.exports = nextConfig
