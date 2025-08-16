5048f21e3d482c04eb163baf8a2c635a95dd147d
priyanshoe <priyanshurahate@gmail.com>
2025-08-16 21:21:18 +0530

    msg

--- app/components/Strip.tsx
+++ app/components/Strip.tsx
@@ -3,20 +3,20 @@ import gsap from "gsap";
 import { useEffect, useRef } from "react";
 
 const ImageData = [
-    { id: 1, name: 'AVIS', imgSrc: '../SVG/AVIS.svg' },
-    { id: 2, name: 'Baron', imgSrc: '../SVG/Baron.svg' },
-    { id: 3, name: 'Clover', imgSrc: '../SVG/Clover.svg' },
-    { id: 4, name: 'Comcast', imgSrc: '../SVG/Comcast.svg' },
-    { id: 5, name: 'ETS', imgSrc: '../SVG/ETS.svg' },
-    { id: 6, name: 'Expedia', imgSrc: '../SVG/Expedia.svg' },
-    { id: 7, name: 'Identity', imgSrc: '../SVG/Identity.svg' },
-    { id: 8, name: 'Lucid', imgSrc: '../SVG/Lucid.svg' },
-    { id: 9, name: 'Microsoft', imgSrc: '../SVG/Microsoft.svg' },
-    { id: 10, name: 'NEOM', imgSrc: '../SVG/NEOM.svg' },
-    { id: 11, name: 'NFL', imgSrc: '../SVG/NFL.svg' },
-    { id: 12, name: 'Toefl', imgSrc: '../SVG/Toefl.svg' },
-    { id: 13, name: 'Under_armour', imgSrc: '../SVG/Under_armour.svg' },
-    { id: 14, name: 'Zappos', imgSrc: '../SVG/Zappos.svg' },
+    { id: 1, name: 'AVIS', imgSrc: '/SVG/AVIS.svg' },
+    { id: 2, name: 'Baron', imgSrc: '/SVG/Baron.svg' },
+    { id: 3, name: 'Clover', imgSrc: '/SVG/Clover.svg' },
+    { id: 4, name: 'Comcast', imgSrc: '/SVG/Comcast.svg' },
+    { id: 5, name: 'ETS', imgSrc: '/SVG/ETS.svg' },
+    { id: 6, name: 'Expedia', imgSrc: '/SVG/Expedia.svg' },
+    { id: 7, name: 'Identity', imgSrc: '/SVG/Identity.svg' },
+    { id: 8, name: 'Lucid', imgSrc: '/SVG/Lucid.svg' },
+    { id: 9, name: 'Microsoft', imgSrc: '/SVG/Microsoft.svg' },
+    { id: 10, name: 'NEOM', imgSrc: '/SVG/NEOM.svg' },
+    { id: 11, name: 'NFL', imgSrc: '/SVG/NFL.svg' },
+    { id: 12, name: 'Toefl', imgSrc: '/SVG/Toefl.svg' },
+    { id: 13, name: 'Under_armour', imgSrc: '/SVG/Under_armour.svg' },
+    { id: 14, name: 'Zappos', imgSrc: '/SVG/Zappos.svg' },
 ];
 
 
