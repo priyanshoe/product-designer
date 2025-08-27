41d9a1c82ca30530b460e088d281c1311d444a69
priyanshoe <priyanshurahate@gmail.com>
2025-08-27 23:08:49 +0530

    added dist folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+'use client'
+import Lenis from 'lenis'
 import Strip from "./components/Strip";
 import Landing from "./(pages)/Landing";
 import Page2 from "./(pages)/Page2";
 import Page3 from "./(pages)/Page3";
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  useEffect(() => {
+    
+    const lenis = new Lenis();
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+    
+      return () => {
+      lenis.destroy()
+    }
+  },[])
+
   return (
     <div className="overflow-hidden">
       <Landing />
