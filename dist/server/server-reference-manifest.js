a4952e28e5421b5f55902e0fabc5bdba1879f032
priyanshoe <priyanshurahate@gmail.com>
2025-08-27 22:07:50 +0530

    file changed

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GDoMGAL1ABrudWYvm7rtvYX/HcnJ3G2McSiU2ywI4Co=\"\n}"
--- dist/static/chunks/app_page_tsx_b8cb437f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_e84b91bb._.js",
-  "static/chunks/app_components_Strip_tsx_04cd4f18._.js"
-],
-    source: "dynamic"
-});
