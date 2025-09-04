29bcb447a48ba9377acf248aa3da4913593d4cc8
priyanshoe <priyanshurahate@gmail.com>
2025-09-04 23:14:09 +0530

    navBar done (responsive)

--- app/components/navBar/NavBar.tsx
+++ app/components/navBar/NavBar.tsx
@@ -9,42 +9,78 @@ export default function NavBar() {
     const [fullNav, setFullNav] = useState(false);
     const [hideNav, setHideNav] = useState(-1);
     const navBar = useRef(null);
-
+    const menuButton = useRef(null);
 
     useEffect(()=>{
-        gsap.registerPlugin(ScrollTrigger);
 
+        hideLinksGSAP();
+        gsap.registerPlugin(ScrollTrigger);
+        
         gsap.to(navBar.current,{
             scrollTrigger:{
                 trigger:document.documentElement, 
                 start: "top top",
                 end: "bottom bottom",
-                scrub:true,
                 onUpdate:(self)=>{
                     setHideNav(self.direction);
                     setFullNav(false);
+                    if(self.direction > 0){
+                        hideLinksGSAP();
+                    }
                 }
             }
         })
     },[])
 
+
+    function hideLinksGSAP(){
+        gsap.to('ul li',{
+            y:-100,
+            stagger:.2,
+            duration:.6
+        })
+        gsap.to(menuButton.current,{
+            y:0,
+            delay:.5,
+            duration:.4
+        })
+    }
+
+    function showLinksGSAP(){
+        gsap.to('ul li',{
+            y:'0%',
+            stagger:.2,
+        })
+        gsap.to(menuButton.current,{
+            y:100,
+        })
+    }
+
     
     return (
-        <div ref={navBar} className={`transitionHideNav md:hidden fixed z-999 top-0 left-0 w-full ${hideNav > 0 ? 'translate-y-[-100%]' : 'translate-y-0'} `}>
+        <div ref={navBar} className={`transitionHideNav fixed z-999 top-0 left-0 w-full ${hideNav > 0 ? 'translate-y-[-100%]' : 'translate-y-0'} `}>
 
-            <div className="font-[Bold] flex justify-between items-center py-8 px-6">
-                <div className="text-[4.2vw]">
+            <div className="font-[Bold] flex justify-between items-center py-8 px-6 xl:px-16">
+                <div className="text-[4.2vw] md:text-[2.1vw] lg:text-[1.6vw] xl:text-[1.2vw] 2xl:text-[.9vw]">
                     <h1>Cynthia Ugwu</h1>
                 </div>
-                <div className="nav-links">
-                    <ul className="hidden justify-between items-center gap-4">
-                        <li>Home</li>
+
+
+                <div className="nav-links relative overflow-hidden sm:w-2/6 lg:w-2/7 xl:w-2/9 2xl:w-2/10  ">
+                    <ul className="max-sm:hidden uppercase text-[1.8vw] lg:text-[1.5vw] lg:pr-2 xl:text-[1.1vw] xl:pr-4 2xl:text-[.8vw] absolute left-0 top-0 flex justify-between items-center gap-4 h-full w-full">
+                        <li>Work</li>
+                        <li>Playground</li>
                         <li>Contact</li>
-                        <li>Download</li>
                     </ul>
                     <button 
-                    onClick={()=> setFullNav(true)}
-                    className="text-[4.2vw] flex items-center justify-center ">MENU<span className="text-sm text-[#9c9c9c]">+</span></button>
+                        onClick={()=> setFullNav(true)}
+                        className="sm:hidden text-[4.2vw] flex items-center justify-center ">MENU<span className="text-sm text-[#9c9c9c]">+</span>
+                    </button>
+                    <button 
+                        onClick={()=> showLinksGSAP()}
+                        ref={menuButton}
+                        className="max-sm:hidden w-full text-[4.2vw] md:text-[2.1vw] lg:text-[1.6vw] xl:text-[1.2vw] 2xl:text-[.9vw] flex items-center justify-end ">MENU<span className="text-sm text-[#9c9c9c]">+</span>
+                    </button>
                 </div>
             </div>
 
