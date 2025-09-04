a906edd59295e4ca1aeb323910ef062876a53896
priyanshoe <priyanshurahate@gmail.com>
2025-09-04 20:47:57 +0530

    navBar done for mobile version

--- app/components/navBar/NavBar.tsx
+++ app/components/navBar/NavBar.tsx
@@ -1,13 +1,36 @@
 'use client';
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import FullScreenNavBar from "./FullScreenNavBar";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function NavBar() {
     
-    const [fullNav, setFullNav] = useState(false)
+    const [fullNav, setFullNav] = useState(false);
+    const [hideNav, setHideNav] = useState(-1);
+    const navBar = useRef(null);
+
+
+    useEffect(()=>{
+        gsap.registerPlugin(ScrollTrigger);
+
+        gsap.to(navBar.current,{
+            scrollTrigger:{
+                trigger:document.documentElement, 
+                start: "top top",
+                end: "bottom bottom",
+                scrub:true,
+                onUpdate:(self)=>{
+                    setHideNav(self.direction);
+                    setFullNav(false);
+                }
+            }
+        })
+    },[])
+
     
     return (
-        <div className=" fixed z-999 top-0 left-0 w-full h-full ">
+        <div ref={navBar} className={`transitionHideNav md:hidden fixed z-999 top-0 left-0 w-full ${hideNav > 0 ? 'translate-y-[-100%]' : 'translate-y-0'} `}>
 
             <div className="font-[Bold] flex justify-between items-center py-8 px-6">
                 <div className="text-[4.2vw]">
